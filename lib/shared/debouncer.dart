25d8b1c24683135c3fd63566b9ead3f46f400aee
ritamk <ritamkonar7@gmail.com>
2021-09-08 14:36:57 +0530

    fixed a little issue with the debouncer

--- lib/shared/debouncer.dart
+++ lib/shared/debouncer.dart
@@ -4,14 +4,14 @@ import 'package:flutter/cupertino.dart';
 
 class Debouncer {
   final int milliseconds;
-  late VoidCallback voidCallback;
-  late Timer _timer;
+  VoidCallback? voidCallback;
+  Timer? _timer;
 
   Debouncer({required this.milliseconds});
 
   run(VoidCallback action) {
     if (_timer != null) {
-      _timer.cancel();
+      _timer!.cancel();
     }
 
     _timer = Timer(Duration(milliseconds: milliseconds), action);
